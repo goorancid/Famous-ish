9387980cd43f0acff8c90bdeeaca8e047eb88d3e
Goorancid <goorancid@hotmail.com>
2015-12-07 23:48:12 -0800

    Redirect frontend
    Moved login from logginButtons to /login on the site,
    working to edit accounts

--- server/publish.js
+++ server/publish.js
@@ -8,3 +8,12 @@ Meteor.publish('secrets', function (group) {
         this.stop();
     }
 });
+
+Accounts.validateNewUser(function (user) {
+    var loggedInUser = Meteor.user();
+    if (Roles.userIsInRole(loggedInUser, ['admin','manage-users'])) {
+        // NOTE: This example assumes the user is not using groups.
+        return true;
+    }
+    throw new Meteor.Error(403, "Not authorized to create new users");
+});
